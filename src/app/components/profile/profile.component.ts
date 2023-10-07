e47d2477b339649b82554052bfca24538583b01e
Nariman <n.murzakaev@big3.ru>
2023-10-07 19:07:24 +0300

    feat: add userService

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,4 +1,6 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
+import { UserService } from 'src/app/providers';
 
 @Component({
   selector: 'app-profile',
@@ -6,9 +8,24 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
   styleUrls: ['./profile.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ProfileComponent {
-  @Input() name = 'Eduard';
-  @Input() image = 'assets/images/avatar.jpg';
+export class ProfileComponent implements OnInit, OnDestroy {
   @Input() avatarWidth = '32px';
   @Input() avatarHeight = '32px';
+
+  private destroyStream$ = new Subject<void>();
+
+  user$ = this.userSerice.user$;
+
+  constructor(private userSerice: UserService) {}
+
+  ngOnInit(): void {
+    this.userSerice.initUser$().pipe(
+      takeUntil(this.destroyStream$),
+    ).subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.destroyStream$.next();
+    this.destroyStream$.complete();
+  }
 }
