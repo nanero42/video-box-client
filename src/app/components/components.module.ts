6831cda02c30fae8940133c64f8b1f0e4710f8bd
Nariman <n.murzakaev@big3.ru>
2023-10-07 15:50:54 +0300

    feat: add icon component

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header';
+import { IconComponent } from './icon';
 
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
+    IconComponent,
   ],
   imports: [
     CommonModule
   ],
   exports: [
     HeaderComponent,
+    IconComponent,
   ]
 })
 export class ComponentsModule { }
---
+++ src/app/components/icon/icon.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-icon',
+  templateUrl: './icon.component.html',
+  styleUrls: ['./icon.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class IconComponent {
+
+}
