55d4d2cd49a88f93b7f66adcad03db3f7d279f22
Nariman <n.murzakaev@big3.ru>
2023-10-07 15:00:32 +0300

    add home & error routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./pages').then(m => m.HomeModule) },
+  {
+    path: '**',
+    pathMatch: 'full',
+    loadChildren: () => import('./pages').then(m => m.ErrorModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
