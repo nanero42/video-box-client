02fe01bda62ec439f8b6c72b550d4d2eeeed891b
Nariman <n.murzakaev@big3.ru>
2023-10-09 21:04:51 +0300

    feat: add takeScreenshot func

--- src/app/providers/utils/index.ts
+++ src/app/providers/utils/index.ts
@@ -10,3 +10,35 @@ export function secondsToHHMMSS(totalSeconds: number) {
 
   return hours + ':' + minutes + ':' + seconds;
 }
+
+export function takeScreenshot(video: HTMLVideoElement) {
+  const w = video.videoWidth;
+  const h = video.videoHeight;
+  const canvas = document.createElement('canvas');
+  canvas.width = w;
+  canvas.height = h;
+  const ctx = canvas.getContext('2d');
+  ctx?.drawImage(video, 0, 0, w, h);
+  const encodedImg = canvas.toDataURL('image/jpg');
+
+  // document.body.appendChild(canvas);
+
+  /*
+  // shorten url like this: blob:http://localhost:4200/89ed9e3a-5241-46f9-ba8a-3c517f2bbcf3
+
+  const output = data.replace(/^data:image\/(png|jpg);base64,/, "");
+
+  const byteCharacters = atob(output);
+  const byteNumbers = new Array(byteCharacters.length);
+  for (let i = 0; i < byteCharacters.length; i++) {
+    byteNumbers[i] = byteCharacters.charCodeAt(i);
+  }
+  const byteArray = new Uint8Array(byteNumbers);
+  const blob = new Blob([byteArray], {type: 'image/gif'});
+  const imageUrl = URL.createObjectURL(blob);
+  */
+
+  // window.open(imageUrl, '_blank');
+
+  return encodedImg;
+}
