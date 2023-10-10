5a458d42c2d1dc0b86d6cfa7c378a454f84f20af
Nariman <n.murzakaev@big3.ru>
2023-10-10 23:12:23 +0300

    refactor: rename variables

--- src/app/providers/utils/index.ts
+++ src/app/providers/utils/index.ts
@@ -1,14 +1,14 @@
 export function secondsToHHMMSS(totalSeconds: number) {
-  let hours: any = Math.floor(totalSeconds / 3600);
-  let minutes: any = Math.floor((totalSeconds - (hours * 3600)) / 60);
-  let seconds: any = Math.floor(totalSeconds - (hours * 3600) - (minutes * 60));
+  let h: any = Math.floor(totalSeconds / 3600);
+  let m: any = Math.floor((totalSeconds - (h * 3600)) / 60);
+  let s: any = Math.floor(totalSeconds - (h * 3600) - (m * 60));
 
   // Padding the values to ensure they are two digits
-  if (hours < 10) { hours = "0" + hours; }
-  if (minutes < 10) { minutes = "0" + minutes; }
-  if (seconds < 10) { seconds = "0" + seconds; }
+  if (h < 10) { h = '0' + h; }
+  if (m < 10) { m = '0' + m; }
+  if (s < 10) { s = '0' + s; }
 
-  return hours + ':' + minutes + ':' + seconds;
+  return h + ':' + m + ':' + s;
 }
 
 export function takeScreenshot(video: HTMLVideoElement) {
